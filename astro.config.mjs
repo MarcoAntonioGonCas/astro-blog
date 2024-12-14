0888b6b9104aa0220fe579686493e027f67340b3
Marco Antonio <marcoantonigoncas@gmail.com>
2024-12-13 22:01:22 -0600

    Actualizar la URL del sitio en la configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  site:"https://astro-blog-inky.vercel.app/",
+  site:"https://blog-astro-marco.netlify.app",
 });
