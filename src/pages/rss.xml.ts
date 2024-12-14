e9700a0601bc289e29962bebe5fda5adc2c5fda0
Marco Antonio <marcoantonigoncas@gmail.com>
2024-12-13 22:30:39 -0600

    Agregar soporte para Markdown y sanitización de HTML en la generación de RSS

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,11 @@
 import type { APIRoute } from 'astro';
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import MarkdownIt from 'markdown-it';
+import sanitizeHtml from 'sanitize-html';
+
+
+const parser = new MarkdownIt();
 
 export const GET: APIRoute = async({ params, request,site }) => {
 
@@ -12,18 +17,30 @@ export const GET: APIRoute = async({ params, request,site }) => {
         title: 'Blog de Marco',
         // `<description>` campo en el xml generado
         description: 'Un blog de Astro',
+
         // Usa el "site" desde el contexto del endpoint
         // https://docs.astro.build/en/reference/api-reference/#contextsite
         site: site!,
         // Array de `<item>`s en el xml generado
         // Consulta la sección "Generando `items`" para ejemplos utilizando colecciones de contenido y glob imports
-        items: blogPost.map(({data,slug}) => ({
+        items: blogPost.map(({data,slug,body}) => ({
             title: data.title,
             pubDate: data.date,
             description: data.description,
             link: `/posts/${slug}`,
+            content: sanitizeHtml(parser.render(body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+            }),  
+            customData: `<media:content
+                type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+                width="${data.image.width}"
+                height="${data.image.height}"
+                medium="image"
+                url="${site + data.image.src}" />
+            `,
         })),
         // (opcional) inyecta xml personalizado
         customData: `<language>es-mx</language>`,
       });
 }
+
